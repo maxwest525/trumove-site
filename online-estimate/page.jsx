90ec9c11eeb41ae17c8b646c39e242be2b9053e6
maxwest525 <maxw@trumoveinc.com>
2025-12-23 10:13:32 -0500

    Delete online-estimate/page.jsx

--- online-estimate/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function OnlineEstimatePage() {
-  return (
-    <main style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>Online Estimate</h1>
-      <p>Temporary build test page. If you see this, deployment is working.</p>
-    </main>
-  );
-}
