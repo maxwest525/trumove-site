8c3ff9687d183c9df52e4ee2bdd45d0d43c0a86c
maxwest525 <maxw@trumoveinc.com>
2025-12-23 13:05:46 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import SiteShell from "./components/SiteShell";
 
 export const metadata = {
   title: "TruMove",
@@ -8,7 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <SiteShell>{children}</SiteShell>
+      </body>
     </html>
   );
 }
